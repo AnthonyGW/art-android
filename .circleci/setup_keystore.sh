719f717efe92cde33c5cfe309103d509e21db8fc
AnthonyGW <anthony.waithaka@andela.com>
2018-04-04 13:04:03 +0300

    fix(circleci): remove gradle properties stuff

--- .circleci/setup_keystore.sh
+++ .circleci/setup_keystore.sh
@@ -4,12 +4,11 @@ set -ex
 set -o pipefail
 
 export GRADLE_PROPERTIES=$HOME"/repo/gradle.properties"
-export KEYSTORE_PROPERTIES=$HOME"/repo/keystores/keystore.properties"
+export KEYSTORE_PROPERTIES=$HOME"/repo/app/keystores/keystore.properties"
 export STORE_FILE_LOCATION=$HOME"/repo/art-android.jks"
 
 function copyEnvVarsToProperties {
 
-    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
     echo "Keystore Properties should exist at $KEYSTORE_PROPERTIES"
 
     if [ ! -f "$KEYSTORE_PROPERTIES" ]
@@ -22,26 +21,6 @@ function copyEnvVarsToProperties {
         echo "keyPassword=$KEY_PASSWORD" >> ${KEYSTORE_PROPERTIES}
         echo "storeFile=$STORE_FILE" >> ${KEYSTORE_PROPERTIES}
         echo "storePassword=$STORE_PASSWORD" >> ${KEYSTORE_PROPERTIES}
-    fi
-
-    if [ ! -f "$GRADLE_PROPERTIES" ]
-    then
-        echo "${GRADLE_PROPERTIES} does not exist...Creating Properties file"
-
-	echo "API_BASE_URL=$API_BASE_URL" >> ${GRADLE_PROPERTIES}
-	echo "DEV_API_BASE_URL=$DEV_API_BASE_URL" >> ${GRADLE_PROPERTIES}
-	echo "API_KEY=$API_KEY" >> ${GRADLE_PROPERTIES}
-	echo "SERVICE_ACCOUNT_EMAIL=$SERVICE_ACCOUNT_EMAIL" >> ${GRADLE_PROPERTIES}
-
-    fi
-
-    if [ ! -f "$PUBLISH_KEY_FILE" ]
-    then
-        echo "${PUBLISH_KEY_FILE} does not exist...creating properties file"
-
-        touch ${PUBLISH_KEY_FILE}
-
-        echo "$PUBLISH_JSON_KEY" >> ${PUBLISH_KEY_FILE}
     fi
 }
 
